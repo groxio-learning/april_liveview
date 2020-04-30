c0ee422cbc847e708bcb568ed4d8cdb2c7f7c5cf
Bruce Tate <bruce@rapidred.com>
2020-04-30 10:10:24 -0400

    timer with counter

--- counter/lib/counter_web/live/counter_live.ex
+++ counter/lib/counter_web/live/counter_live.ex
@@ -7,28 +7,26 @@ defmodule CounterWeb.CounterLive do
   def mount(_params, _session, socket) do
     # think of assign as "add data to the user area of the live view"
     # the very first thing that the router will call is mount/3
-
-    {:ok, initialize(socket)}
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    
+    {
+      :ok, 
+      socket 
+      |> initialize 
+      |> show
+    }
   end
-
-  def render(%{count: count} = assigns) do
-    # ~L""" <-- this is a sigil
-    ~L"""
-    <div phx-window-keydown="counter">
-    <h1 phx-click="count">
-      <!-- could also be assigns.count -->
-        <%= @count %>
-
-      </h1>
-    </div>
-    """
+  
+  def show(socket) do
+    socket
+    |> assign(:time, Time.utc_now() |> to_string)
   end
-
+  
   defp initialize(socket) do
     # this just adds stuff onto the socket
     assign(socket, count: 0)
   end
-
+  
   defp inc(socket) do
     assign(socket,
         count: socket.assigns.count + 1
@@ -40,14 +38,32 @@ defmodule CounterWeb.CounterLive do
     )
   end
 
+  def render(%{count: count} = assigns) do
+    # ~L""" <-- this is a sigil
+    ~L"""
+    <div phx-window-keydown="counter">
+    <h1 phx-click="count">
+      <!-- could also be assigns.count -->
+        <%= @count %>
+
+      </h1>
+      <h2><%= @time %></h2>
+    </div>
+    """
+  end
+
   def handle_event("count", _metadata, socket) do
-    {:noreply, socket |> inc}
+    {:noreply, socket |> inc |> show}
   end
   def handle_event("counter", %{"key" => "ArrowUp"}, socket) do
-    {:noreply, inc(socket)}
+    {:noreply, socket |> inc |> show}
   end
   def handle_event("counter", %{"key" => "ArrowDown"}, socket) do
-    {:noreply, dec(socket)}
+    {:noreply, socket |> dec |> show}
+  end
+  
+  def handle_info(:tick, socket) do
+    {:noreply, socket |> show}
   end
 
 end
